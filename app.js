e13847bf0b9f73b7eaad2e6c479738dba319a3e7
allahrakha79 <154686133+allahrakha79@users.noreply.github.com>
2024-03-07 22:02:48 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+var num1 = +prompt("Enter Your Number");
+var num2 = +prompt("Enter Your Number");
+
+console.log("Additon of Your Numbers is:", num1 + num2);
+console.log("Subtraction of Your Numbers is:", num1 - num2);
+console.log("Multiplication of Your Numbers is", num1 * num2)
+console.log("Division of Your Numbers is:", num1 / num2);
+console.log("Remainder of Your Numbers is:", num1 % num2);
+
+
+
+
+document.write("Additon of Your Numbers is:", num1 + num2,"<br />");
+document.write("Subtraction of Your Numbers is:", num1 - num2, "<br />");
+document.write("Multiplication of Your Numbers is", num1 * num2, "<br />")
+document.write("Division of Your Numbers is:", num1 / num2, "<br />");
+document.write("Remainder of Your Numbers is:", num1 % num2);
